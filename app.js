88552ad6a4a09efb3386d5f55131623328848d1c
devarnab <arnabchattopadhyay.in@gmail.com>
2020-09-06 23:32:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from './webpack.config.dev.js';
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
+
+app.get('/', function (req, res) {
+  res.send('Ok Server running!');
+});
+
+app.get('/test', function (req, res) {
+  res.send('Test route!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
